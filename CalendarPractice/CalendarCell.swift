c90d6cdb70aa85a1f6cae2f2cecc0e4d000bf982
EhsanTgv <EhsanTgv@gmail.com>
2022-02-19 20:03:19 +0300

    Implement design

---
+++ CalendarPractice/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  CalendarPractice
+//
+//  Created by İbrahim on 19.02.2022.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell{
+    
+    @IBOutlet weak var dayOfMonth:UILabel!
+}
--- CalendarPractice/ViewController.swift
+++ CalendarPractice/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var monthLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func previousMonth(_ sender: Any) {
+    }
+    
+    @IBAction func nextMonth(_ sender: Any) {
+    }
 }
 
